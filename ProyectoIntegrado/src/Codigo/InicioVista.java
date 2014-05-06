dd2e2b74ee3e31f86327ccbeeb123d4b0f07773b
fer2k14 <aralfer2013@gmail.com>
2014-05-06 17:28:11 +0200

    Pantalla Inicio medio terminada

--- ProyectoIntegrado/src/Codigo/InicioVista.java
+++ ProyectoIntegrado/src/Codigo/InicioVista.java
@@ -6,6 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.SystemColor;
+import java.awt.Font;
 
 public class InicioVista extends JFrame {
 
@@ -35,8 +43,27 @@ public class InicioVista extends JFrame {
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnCrearTest = new JButton("Crear Test");
+		btnCrearTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnCrearTest.setBounds(306, 309, 171, 43);
+		contentPane.add(btnCrearTest);
+		
+		JButton btnMisTest = new JButton("Mis Test");
+		btnMisTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnMisTest.setBounds(306, 377, 171, 43);
+		contentPane.add(btnMisTest);
+		
+		JButton btnDemoTest = new JButton("Demo Test");
+		btnDemoTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnDemoTest.setBounds(306, 444, 171, 43);
+		contentPane.add(btnDemoTest);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(InicioVista.class.getResource("/Imagenes/fondo_aplicacion.jpg")));
+		lblNewLabel.setBounds(0, 0, 784, 562);
+		contentPane.add(lblNewLabel);
 	}
-
 }
