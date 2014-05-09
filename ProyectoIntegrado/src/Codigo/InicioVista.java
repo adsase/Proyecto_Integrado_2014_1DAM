4cdd622f9a7a9701d65812bda12de447d7011fe7
fer2k14 <aralfer2013@gmail.com>
2014-05-09 18:13:23 +0200

    imagenes actualizadas

--- ProyectoIntegrado/src/Codigo/InicioVista.java
+++ ProyectoIntegrado/src/Codigo/InicioVista.java
@@ -11,13 +11,23 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import java.awt.Color;
 import java.awt.SystemColor;
 import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class InicioVista extends JFrame {
 
 	private JPanel contentPane;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -34,11 +44,24 @@ public class InicioVista extends JFrame {
 			}
 		});
 	}
+	
+	public void setGraphics () {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public InicioVista() {
+		setGraphics ();
+		
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
@@ -46,21 +69,6 @@ public class InicioVista extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JButton btnCrearTest = new JButton("Crear Test");
-		btnCrearTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		btnCrearTest.setBounds(306, 309, 171, 43);
-		contentPane.add(btnCrearTest);
-		
-		JButton btnMisTest = new JButton("Mis Test");
-		btnMisTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		btnMisTest.setBounds(306, 377, 171, 43);
-		contentPane.add(btnMisTest);
-		
-		JButton btnDemoTest = new JButton("Demo Test");
-		btnDemoTest.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		btnDemoTest.setBounds(306, 444, 171, 43);
-		contentPane.add(btnDemoTest);
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon(InicioVista.class.getResource("/Imagenes/fondo_aplicacion.jpg")));
